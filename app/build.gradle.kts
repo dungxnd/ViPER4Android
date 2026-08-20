41cc0a3dab062e772eae61a6e8b04f7c7dfd1bcf
Dung Nguyen <158302785+DungxND@users.noreply.github.com>
2026-08-20 15:26:26 +0700

    chore: bump version to 2.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 20260820
-        versionName = "2.3.9"
+        versionName = "2.4.0"
     }
 
     androidResources {
